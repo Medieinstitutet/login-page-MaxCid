92c8426bc4bb5a2dc7ffe26872a46543380262e3
MaxCid <113206163+MaxCid@users.noreply.github.com>
2022-11-27 20:07:55 +0100

    Äntligen är allt som det ska vara.. det gick så långt att mot slutet så började jag glömma bort vad dom olika rader gjorde.. men tillslut så fick jag till det.

--- main2.js
+++ main2.js
@@ -30,6 +30,9 @@ let loginSessions = JSON.parse(localStorage.getItem("accounts"));
 
 /// storage space for accounts
 
+// (denna kod gör detsamma som nedanför fast den fångar och sparar  den nya användaren
+// men raderar arrayen med förbestämda användare.)
+
 // let accounts = [];
 // if (!localStorage.getItem("accounts")) {
 //   let accounts = [
@@ -42,14 +45,52 @@ let loginSessions = JSON.parse(localStorage.getItem("accounts"));
 //   console.log("localestorages has been created");
 // }
 
-let accounts = [
+// (kod blocken från rad 45 till rad 53 gör så att jag kan fånga arrayen,
+// jag kan även lägga till en ny användare och ha en online session samt uppdatera
+// och vara kvar online när jag har loggat in med en ny skapad användare, problemet 
+// är att när jag uppdaterar sidan i login page så försvinner den nya användaren)
+
+
+//problemet löst med en window.onload funktion... fråga mig inte en hur det gick till...
+window.onload = function () {
+  console.log("Refresh!");
+  localStorage.setItem("accounts", JSON.stringify(loginSessions));
+}
+
+if (localStorage.getItem("loginSession")) {
+  console.log("getting shit works");
+  let username = JSON.parse(localStorage.getItem("loginSession"));
+
+  loginPage.classList.add("formHidden");
+  accountPage.classList.remove("formHidden");
+
+  console.log("USERNAME:", username);
+  userOnline.innerHTML = `Hello ${username.user}!`;
+}
+
+let accounts = [];
+
+if (localStorage.getItem("loginSessions")){
+  accounts = JSON.parse(localStorage.getItem("accounts"));
+  localStorage.setItem("accounts", JSON.stringify(accounts));
+}else{
+  accounts = [
     {id:1, user:'janne', password:"test",},
     {id:2, user:'PirateKing', password: "1.piece",},
     {id:3, user:'dayman', password:"nightman",},
     {id:4, user:'fighterofthe', password:'nightman96',},
     ];
-localStorage.setItem("accounts", JSON.stringify(accounts));
+  localStorage.setItem("accounts", JSON.stringify(accounts));
+}
+
+
 console.log("localestorages has been created");
+
+
+
+
+
+
 
 // login page (get a verification from the inputs and change page)
 
@@ -57,17 +98,7 @@ loginBtn.addEventListener("click", () => {
   loggedIn();
 });
 
-if (localStorage.getItem("loginSession")) {
-  console.log("getting shit works");
-  let username = JSON.parse(localStorage.getItem("loginSession"));
-  let accounts = JSON.parse(localStorage.getItem("accounts"));
-  accounts.push(loginsessions);
-  localStorage.setItem("accounts", JSON.stringify(accounts));
-  loginPage.classList.add("formHidden");
-  accountPage.classList.remove("formHidden");
-  console.log("USERNAME:", username);
-  userOnline.innerHTML = `Hello ${username.user}!`;
-}
+
 
 function loggedIn() {
   let accounts = JSON.parse(localStorage.getItem("accounts"));
